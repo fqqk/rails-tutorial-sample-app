0a1310b8c544111083ce8faf985094aa3422ae8a
fqqk <fqqk.work68@gmail.com>
2022-07-18 14:02:36 +0900

    userモデルのRspecテスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,4 +63,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.filter_run_when_matching :focus
+  include ApplicationHelper
 end
